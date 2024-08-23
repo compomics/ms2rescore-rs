2fc719b3bb29c3552a917b4d84ae98b33129be56
Kevin Velghe <kevin.velghe@ugent.be>
2024-08-23 14:13:39 +0200

    add function for file type check

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,17 @@ use file_types::{match_file_type, SpectrumFileType};
 use precursor::Precursor;
 use ms2_spectrum::MS2Spectrum;
 
+/// Check if spectrum path matches a supported file type.
+#[pyfunction]
+pub fn is_supported_file_type(spectrum_path: String) -> bool {
+    let file_type = match_file_type(&spectrum_path);
+
+    match file_type {
+        SpectrumFileType::Unknown => false,
+        _ => true
+    }
+}
+
 /// Get mapping of spectrum identifiers to precursor information.
 #[pyfunction]
 pub fn get_precursor_info(spectrum_path: String) -> PyResult<HashMap<String, Precursor>> {
@@ -57,6 +68,7 @@ pub fn get_ms2_spectra(spectrum_path: String) -> PyResult<Vec<ms2_spectrum::MS2S
 fn ms2rescore_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Precursor>()?;
     m.add_class::<MS2Spectrum>()?;
+    m.add_function(wrap_pyfunction!(is_supported_file_type, m)?)?;
     m.add_function(wrap_pyfunction!(get_precursor_info, m)?)?;
     m.add_function(wrap_pyfunction!(get_ms2_spectra, m)?)?;
     Ok(())
